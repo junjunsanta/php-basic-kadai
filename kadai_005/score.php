c50ca078dbc67fb03ad57e5eea06727fba4915d5
junjunsanta <buuu.hi4645@gmail.com>
2025-03-26 17:29:55 +0900

    フォルダをアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題005</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 変数に値を代入する
+    $score1 = '80';
+
+    $score2 = '60';
+
+    $score3 = '55';
+
+    $score4 = '40';
+
+    $score5 = '100';
+
+    $score6 = '25';
+
+    $score7 = '80';
+
+    $score8 = '95';
+
+    $score9 = '30';
+
+    $score10 = '60';
+
+    // 変数に数値を計算しその値を出力する
+    echo $score1 +  $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10 ;
+
+    // 変数に値を代入する
+    $total = $score1 +  $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10 ;
+
+    echo '<br>';
+
+    // 変数に数値を計算しその値を出力する
+    echo $total /10;
+
+
+    ?>
+  </p>
+</body>
+</html>
