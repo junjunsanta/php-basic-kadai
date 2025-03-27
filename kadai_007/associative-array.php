bef5514542a73ec7ced490c07cb98aee1d906590
junjunsanta <buuu.hi4645@gmail.com>
2025-03-27 11:00:22 +0900

    フォルダをアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+<body>
+  <?php
+  //連想配列に値を代入する
+  $personal_data = ['name' =>'onion','price' => 200,'weight' => 160,];
+
+  //値を出力する
+  print_r($personal_data);
+  
+  ?>
+</body>
+</html>
